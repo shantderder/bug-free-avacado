984002bb686ad2cc445caa310a6fb0f95cbde23e
Shant Derderian <shantderderian@gmail.com>
2017-08-15 18:13:10 -0700

    Feline Addon

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "meow.h"  /* Include the header here, to obtain the function declaration */
+
+int main(void)
+{
+    int y = meow(3);  /* Use the function here */
+    printf("%d\n", y);
+    return 0;
+}
---
+++ meow.c
@@ -0,0 +1,6 @@
+#include "meow.h"  /* Include the header (not strictly necessary here) */
+
+int meow(int x)    /* Function definition */
+{
+    return x + 5;
+}
